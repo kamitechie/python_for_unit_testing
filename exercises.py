b99b8ed1011c2d25ef011d190e716a6f072c0a07
kamitechie <kamitechie@gmail.com>
2023-05-30 07:29:49 +0300

    Changed exercises and added tests

--- exercises.py
+++ exercises.py
@@ -1,25 +1,17 @@
-import math
-
 # Python exercises
 
-def exercise_reverse_name():
-    name = input("Enter your name: ")
-    last_name = input("Enter your last name: ")
-    full_name = name + " " + last_name
+
+def exercise_reverse_name(full_name):
     reverse_name = full_name[::-1]
-    print(reverse_name)
+    return reverse_name
 
 
-def exercise_num_to_tuple():
-    numbers = input("Enter numbers: ")
-    numbers_list = numbers.split(",")
-    numbers_tuple = tuple(numbers_list)
-    print(f"List: {numbers_list}\nTuple: {numbers_tuple}")
+def exercise_file_ext(file_name):
+    file = file_name.split(".")
+    file_ext = file[-1]
+    return file_ext
 
 
-def exercise_sphere_volume():
-    r = 6
-    volume = (4/3)*math.pi*r**3
-    return print(f"Sphere volume: {volume}")
+
 
 
--- test_exercises.py
+++ test_exercises.py
@@ -1,2 +1,15 @@
 import pytest
+import exercises
+
+
+def test_exercise_reverse_name():
+    assert exercises.exercise_reverse_name("John Smith") == "htimS nhoJ"
+    assert exercises.exercise_reverse_name("ANNA TAO") == "OAT ANNA"
+
+
+def test_exercise_file_ext():
+    assert exercises.exercise_file_ext('main.py') == 'py'
+    assert exercises.exercise_file_ext('file.png') == 'png'
+    assert exercises.exercise_file_ext('FILE.html') == 'html'
+
 
